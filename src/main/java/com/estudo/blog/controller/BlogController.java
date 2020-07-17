013b9c80d4ccc39e532243e56bba330a1a7a316d
Diego Bizarelo <diego.bizarelo@gmail.com>
2020-07-16 22:12:43 -0300

    Adicionado Formulário para nova postagem

--- src/main/java/com/estudo/blog/controller/BlogController.java
+++ src/main/java/com/estudo/blog/controller/BlogController.java
@@ -2,13 +2,18 @@ package com.estudo.blog.controller;
 
 import com.estudo.blog.model.Post;
 import com.estudo.blog.service.BlogService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,4 +36,20 @@ public class BlogController {
         mv.addObject("post", post);
         return mv;
     }
+
+    @RequestMapping(value = "/novopost", method = RequestMethod.GET)
+    public String formPost() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/novopost", method = RequestMethod.POST)
+    public String savePost(@Validated Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/novopost";
+        }
+        post.setData(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
+    }
+
 }
